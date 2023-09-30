bc27830cbdf43ade5d57e0f20253d04477b5c676
YuuY314 <rafaelyu0202@gmail.com>
2023-09-30 01:43:27 -0300

    Add Connection to MongoDB

---
+++ src/database/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@tutorialnodejs.pjyktue.mongodb.net/?retryWrites=true&w=majority`)
+        .then(console.log("Conexão efetuada com sucesso!"))
+        .catch(error => {
+            console.log('Ocorreu um erro ao realizar a conexão! Erro: ', error)
+        });
+}
+
+module.exports = connectToDatabase; 
